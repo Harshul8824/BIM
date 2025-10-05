e6a7e54de858939841cdfacbd9145e6122d5826f
Harshul Goswami <goswamiharshul88@gmail.com>
2025-10-06 00:11:06 +0530

    update api url

--- frontend/src/components/ProgressList.jsx
+++ frontend/src/components/ProgressList.jsx
@@ -56,7 +56,7 @@ const ProgressList = ({ project, onBack, onAddProgress }) => {
       console.log('Sending data to ML model:', data);
 
       // Send data to Flask ML model API
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://bim-rwba.onrender.com/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
